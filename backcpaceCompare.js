dac1387ee054d8db2408e9724a3fc29c95831b69
Svvetlana <schsvetik74@gmail.com>
2022-08-14 14:57:57 +0300

    add day14

---
+++ backcpaceCompare.js
@@ -0,0 +1,13 @@
+/* Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".*/
+
+var backspaceCompare = function(s, t) {
+    const cleanString = (str) => 
+    !str.includes("#") ? str : cleanString(str.replace(/.?#/, ""));
+    return cleanString(s) == cleanString(t);
+};
